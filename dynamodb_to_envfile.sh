b105931e187cddbfa1764c77b9ff5fec0181e566
Mitch Dempsey <mrdempsey@gmail.com>
2015-07-05 19:44:28 -0700

    Increase timeout for metadata

--- dynamodb_to_envfile.sh
+++ dynamodb_to_envfile.sh
@@ -3,18 +3,27 @@
 set -o pipefail
 
 dynamo_table=${DYNAMODB_TABLE?"You must provide a table name"}
+dynamo_region=${DYNAMODB_REGION-"us-east-1"}
 
 out_file=${OUTPUT_FILE-"environment"}
 
-export AWS_DEFAULT_REGION=${DYNAMODB_REGION-"us-east-1"}
+access_key=${AWS_ACCESS_KEY-none}
+secret_key=${AWS_SECRET_KEY-none}
 
-aws_access_key=${AWS_ACCESS_KEY-none}
-aws_secret_key=${AWS_SECRET_KEY-none}
-
-if [[ $aws_access_key != "none" && $aws_secret_key != "none" ]]; then
-  export AWS_ACCESS_KEY_ID=$aws_access_key
-  export AWS_SECRET_ACCESS_KEY=$aws_secret_key
+if [[ $access_key != "none" && $secret_key != "none" ]]; then
+  aws configure set aws_access_key_id $access_key
+  aws configure set aws_secret_access_key $secret_key
+else
+  # Set these to prevent things from timing out
+  # default is 1 for timeout and 1 for attempts
+  #
+  # On EC2, these should not fail, so be generous with the attempts
+  aws configure set metadata_service_timeout ${EC2_METADATA_TIMEOUT-8}
+  aws configure set metadata_service_num_attempts ${EC2_METADATA_ATTEMPTS-10}
 fi
+
+# Set the region
+aws configure set default.region $dynamo_region
 
 key_name=${KEY_ATTRIBUTE-Variable}
 value_name=${VALUE_ATTRIBUTE-Value}
@@ -25,7 +34,7 @@ echo -n > /tmp/exported_env
 
 echo "# Exported by dynamodb-to-envfile on $(date)" > /tmp/exported_env
 
-aws dynamodb scan --region $AWS_DEFAULT_REGION --table-name $dynamo_table --color off --output json \
+aws dynamodb scan --region $dynamo_region --table-name $dynamo_table --color off --output json \
   | jq --raw-output ".Items | map(.${key_name}.S + \"=\" + .${value_name}.S) | .[]" \
   >> /tmp/exported_env
 retval=$?
